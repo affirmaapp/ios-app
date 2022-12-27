57ed707e5ae2ed79f589edc64ec49e44d5aaedb5
Vidushi <vidushi.jaiswal@airblack.co>
2022-12-27 19:09:39 +0530

    (feat): welcome screen added

---
+++ Affirma/Base/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  Affirma
+//
+//  Created by Airblack on 26/12/22.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
---
+++ Affirma/Onboarding/WelcomeScreenViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeScreenViewController.swift
+//  Affirma
+//
+//  Created by Airblack on 26/12/22.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeScreenViewControllerFactory: NSObject {
+    class func produce() -> WelcomeScreenViewController {
+        let welcomeVC = WelcomeScreenViewController(nibName: "WelcomeScreenViewController",
+                                                    bundle: nil)
+        return welcomeVC
+    }
+}
+
+class WelcomeScreenViewController: BaseViewController {
+    @IBOutlet private weak var subtextLabel: UILabel!
+    @IBOutlet private weak var arrowDownImage: UIImageView!
+    @IBOutlet private weak var bottomConstraint: NSLayoutConstraint!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUI()
+
+    }
+    
+    private func setUI() {
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            self.subtextLabel.alpha = 1
+        }
+        
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveLinear, .repeat, .autoreverse]) {
+            
+            self.arrowDownImage.frame = self.arrowDownImage.frame.insetBy(dx: 2, dy: 2)
+            
+        }
+    }
+}
